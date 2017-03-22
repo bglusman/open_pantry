300142c49c48f6fba2890f982c350a624c27f686
Brian Glusman <brian@stellaservice.com>
2017-03-22 19:25:15 -0400

    @keathleys config for circleci

--- config/test.exs
+++ config/test.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
-defmodule Ownership do
+defmodule OpenPantry.Ownership do
   def timeout do
     try do
       String.to_integer(System.get_env("DB_TIMEOUT"))
@@ -9,6 +9,38 @@ defmodule Ownership do
     end
   end
 end
+
+
+defmodule OpenPantry.WallabyConfig do
+  @ci "CI"
+  @circle_artifacts "CIRCLE_ARTIFACTS"
+
+  def timeout() do
+    cond do
+      ci?() -> 30_000
+      true -> 5_000
+    end
+  end
+
+  def screenshot_dir() do
+    cond do
+      ci?() -> System.get_env(@circle_artifacts)
+      true -> "priv/wallaby/screenshots"
+    end
+  end
+
+  def ci?() do
+    !!System.get_env(@ci)
+  end
+end
+
+
+config :wallaby,
+  screenshot_on_failure: true,
+  js_errors: true,
+  js_logger: :stdio,
+  screenshot_dir: OpenPantry.WallabyConfig.screenshot_dir(),
+  max_wait_time: OpenPantry.WallabyConfig.timeout()
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
@@ -29,5 +61,5 @@ config :open_pantry, OpenPantry.Repo,
   password: "postgres",
   database: "open_pantry_test",
   hostname: "localhost",
-  ownership_timeout: Ownership.timeout,
+  ownership_timeout: OpenPantry.Ownership.timeout,
   pool: Ecto.Adapters.SQL.Sandbox
